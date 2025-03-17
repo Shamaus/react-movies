479e67f78539cbef429197390be35f94051c4f93
Vadim Borisenko <Shamaus@bk.ru>
2025-03-17 11:11:40 +0300

    project complete

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer red lighten-2">
+        <div className="footer-copyright">
+            <div className="container">
+                © {new Date().getFullYear()} Copyright Text
+                
+            </div>
+        </div>
+    </footer>
+}
+
+export { Footer };
